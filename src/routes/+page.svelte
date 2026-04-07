dd868c15599c2a5a740a09916a4ffabe2b282c13
Hristiyan Stefanov <stefanovh@aforbes.com>
2026-04-07 11:58:15 +0300

    tiny change to test cicd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Hristiyan's first SvelteKit app, containerized and hosted on a Render Web Service using GitHub Actions for CI/CD. :)</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
